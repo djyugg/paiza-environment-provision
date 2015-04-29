c0b596f201cec44a953c1dc28535e6d54d62de9a
djyugg <sv03@n741x1.com>
2015-04-29 19:29:23 +0900

    vagrant provision時にAnsibleを実行してRuby(2.0.0-p645)をインストール

--- provision.sh
+++ provision.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
+# OSインストール後にすべてのパッケージをアップデートする
 sudo apt-get update -y
 
+# Ansibleのインストール
 sudo apt-get install software-properties-common -y
 sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update -y
 sudo apt-get install ansible -y
+
+# Ansible実行
+cd /vagrant/ansible
+ansible-galaxy install -r requirements.yml -p ./roles
+ansible-playbook site.yml
